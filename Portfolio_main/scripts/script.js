e0833907832a1acfdc33393bc60e1d6e5250b3ae
nayeon000 <na98729@naver.com>
2022-08-03 16:42:47 +0900

    코드수정중

--- Portfolio_main/scripts/script.js
+++ Portfolio_main/scripts/script.js
@@ -16,15 +16,33 @@ document.body.addEventListener('mousemove', (e) => {
 function ani() {
   requestAnimationFrame(ani);
 
-  circleX += (mouseX - circleX) * 0.1;
-  circleY += (mouseY - circleY) * 0.1;
+  circleX += (mouseX - circleX) * 0.2;
+  circleY += (mouseY - circleY) * 0.2;
 
   circle.style.transform = `translate(${circleX}px,${circleY}px)`;
 }
 
 ani();
 
+
 // 스크롤시 한페이지식
 
-// 기본 이벤트 제거 
 
+
+
+// 스크롤시 페이드인 work부분
+const saTriggerMargin = 300;
+const saElementList = document.querySelectorAll('.sa');
+
+const saFunc = function () {
+  for (const element of saElementList) {
+    if (!element.classList.contains('show')) {
+      if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+        element.classList.add('show');
+      }
+    }
+  }
+}
+
+window.addEventListener('load', saFunc);
+window.addEventListener('scroll', saFunc);

393e070682101262c23bd0802b2d7593febb3ed3
nayeon000 <na98729@naver.com>
2022-08-22 10:42:59 +0900

    플라츠 작업

--- platz/scripts/main.js
+++ platz/scripts/main.js
@@ -88,6 +88,8 @@ function slide(item, idx) {
   $('.slide_item').eq(num).addClass('active')
 }
 
+setInterval(auto, 8000)
+
 // nav hover 부분
 let elements = document.querySelectorAll("header nav ul li a");
 
